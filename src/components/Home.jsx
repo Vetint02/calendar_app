ee3b50349534ff9070bb2eaf45d48fda1dbf4bc3
Vetint02 <roy.park.0704@gmail.com>
2026-04-13 04:51:59 -0700

    created visible + invisible squares for the days

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,80 +1,48 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './css/home.css'
 
 export default function Home() {
 
+    let paddingDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let td = new Date();
+    let month = td.getMonth();
+    let year = td.getFullYear();
+
+    let daysInMonth = new Date(year, month + 1, 0).getDate();
+    let startingDay = new Date(year, month, 1).getDay()
+
+    const startingArray = Array.from({ length: startingDay }, (_, i) => i + 1)
+    const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+    
     return (
-        <div>
+        <div className="calendar">
             <h1 style={{ textAlign: 'center', margin: '20px' }}>Calendar App</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Sun</th>
-                        <th>Mon</th>
-                        <th>Tue</th>
-                        <th>Wed</th>
-                        <th>Thu</th>
-                        <th>Fri</th>
-                        <th>Sat</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                    </tr>
-                    <tr>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                        <td>19</td>
-                        <td>20</td>
-                        <td>21</td>
-                    </tr>
-                    <tr>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                        <td>26</td>
-                        <td>27</td>
-                        <td>28</td>
-                    </tr>
-                    <tr>
-                        <td>29</td>
-                        <td>30</td>
-                        <td>31</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="calendar_headers">
+                {
+                    paddingDays.map((day, index) => {
+                        return (
+                            <div key={index} className="head">{day}</div>
+                        )
+                    })
+                }
+            </div>
+            <div className="calendar_contents">
+                {
+                    startingArray.map((day, index) => {
+                        return (
+                            <div key={index} className="padding_box"/>
+                        )
+                    })
+                }
+                {
+                    daysArray.map((day, index) => {
+                        return (
+                            <div key={index} className="calendar_box"></div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
